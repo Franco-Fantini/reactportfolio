7826745f7f6b1f61de5dc998ccbec7a775f2a11b
Franco-Fantini <franco.fantini.1993@gmail.com>
2024-07-05 16:10:52 +0200

    adding grid, and UI to project(bentogrid, floatingnav, shimmerbutton, textgenerateffect)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import { Grid } from "@/components/Grid";
 import Hero from "@/components/Hero";
-
+import { FloatingNav } from "@/components/UI/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
   <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-black/90">
     <div className="max-w-7xl w-full">
+    <FloatingNav
+    navItems={[{name: 'Home', link : '/', icon:<FaHome />}]} />
     <Hero/>
+    <Grid />
     </div>
   </main>
   );
---
+++ components/Grid.tsx
@@ -0,0 +1,25 @@
+import { BentoGridItem , BentoGrid } from './UI/BentoGrid'
+import { gridItems } from '../data/index'
+export const Grid = () => {
+  return (
+    <section id="about">
+        <BentoGrid>
+        {gridItems.map(({ id, title, description , className, img
+            ,imgClassName,titleClassName,spareImg
+        }) => (
+            <BentoGridItem
+            id={id}
+            key={id}
+            title={title}
+            description={description}
+            className={className}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+            />
+        ))}
+        </BentoGrid>
+    </section>
+  )
+}
