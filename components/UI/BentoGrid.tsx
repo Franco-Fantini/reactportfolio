cc90fec625bfb2590ef05e3a358a40ba77488912
Franco-Fantini <franco.fantini.1993@gmail.com>
2024-08-19 18:04:04 +0200

    finishing previus section (bentogridsection) and adding the projects section

--- components/UI/BentoGrid.tsx
+++ components/UI/BentoGrid.tsx
@@ -1,6 +1,12 @@
+"use client";
 import { cn } from "@/lib/util";
-import { BackgroundGradient } from "./BackgroundGradient";
 import { GlobeDemo } from "./GridGlobe";
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
+import ShimmerButton from "./ShimmerButton";
+import { IoCopyOutline } from "react-icons/io5";
+
 export const BentoGrid = ({
   className,
   children,
@@ -41,10 +47,15 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("franco.fantini.1993@gmail.com");
+    setCopied(true);
+  };
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border-transparent justify-between flex flex-col space-y-4 border-2 ",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1] ",
         className
       )}
       style={{
@@ -53,7 +64,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(150,150,182,1) 30%, rgba(27,45,48,1) 100%)",
       }}
     >
-      <div className={`${id === 6} && flex justify-center h-full`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -85,19 +96,59 @@ export const BentoGridItem = ({
           <div className="font-sans font-extralight text-neutral-600 text-sm md:text-xs lg:text-base z-10 dark:text-neutral-300">
             {description}
           </div>
-          <div className="font-sans font-bold text-lg lg:text-2xl max-w-96 z-10 ">
+          <div className="font-sans font-bold text-md lg:text-2xl  z-10 ">
             {title}
           </div>
         </div>
 
         {id === 2 && <GlobeDemo />}
-        {id === 3 && (
-          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 ">
+        {id === 3 &&  (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute top-1 right-1">
             <div className="flex flex-col gap-3 lg:gap-8">
-              {["React JS", "Next.JS", "TypeScript",""].map((item) => (
-                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+              {["React JS", "Javascript", "TypeScript"].map((item) => (
+                <span
+                  key={item}
+                  className="py-2 lg:py4 lg:px-3
+                 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                >
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+            </div>
+            <div className="flex flex-col gap-3 lg:gap-8">
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              {["Postman", "Wordpress", "SQL"].map((item) => (
+                <span
+                  key={item}
+                  className="py-2 lg:py4 lg:px-3
+                 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                >
+                  {item}
+                </span>
               ))}
             </div>
+          </div>
+        )}
+        {id === 5 && (
+          <div className="absolute bottom-5 text-md  sm:bottom-[3rem] md:top-[9rem] md:left-3 lg:left-7 px-5  lg:w-96">
+            <p>
+              Tech and informatic systems are my passion since a early age, so i
+              am always looking for a new knowledge and skills.{" "}
+            </p>
+          </div>
+        )}
+
+        {id === 6 && (
+          <div className={`absolute bottom-10 left-[35%] text-sm`}>
+            <ShimmerButton
+              title={copied ? "Email copied" : "Copy my email"}
+              icon={<IoCopyOutline />}
+              position="left"
+              otherClasses="!bg-[#161a31]"
+              handleClick={handleCopy}
+            />
+            
           </div>
         )}
       </div>
